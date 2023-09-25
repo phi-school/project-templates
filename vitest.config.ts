5b2d5f5e88be3aaf845c4888277d8d9d4ed40f2b
theurgi <58859663+theurgi@users.noreply.github.com>
2023-09-25 11:23:08 -0400

    feat: add create-package script [skip ci]

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		include: ['/packages/*/src/**/*.{test}.{ts,js}'],
+		include: ['/packages/*/src/**/*.{test}.{ts,js}'],		
 	},
 })
